b11c329240095c0eed5444bb206b1440d06bd327
sineththamuditha <sineththamuditha@gmail.com>
2023-09-22 14:22:09 +0530

    Add Mars, Jupiter and Saturn textures

--- app.js
+++ app.js
@@ -216,7 +216,7 @@ class App {
   }
 
   _setPointLight() {
-    const pointLight = new THREE.PointLight(0xffffff, 0.8, 1000);
+    const pointLight = new THREE.PointLight(0xffffff, 1, 1000);
     pointLight.position.set(0, 0, 0);
     this._pointLight = pointLight;
     this._scene.add(pointLight);
